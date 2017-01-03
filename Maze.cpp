233f622ac360f83a510c916134e47018eb2be360
sylxjtu <a443833359@gmail.com>
2017-01-03 13:07:07 +0800

    add comments

--- Maze.cpp
+++ Maze.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Union-Find 节点
 class Node {
   Node* parent;
   Node* findparent () {
@@ -25,6 +26,7 @@ public:
   }
 };
 
+// 连接迷宫节点的边
 class Edge {
   Node* s;
   Node* e;
@@ -42,6 +44,7 @@ public:
   }
 };
 
+// 泛化二维数组
 template < class T >
 class Array2d {
   T* data;
@@ -54,19 +57,26 @@ public:
   ~Array2d () {
     delete[] data;
   }
-  T* get (int x, int y) {
-    return &data[y * width + x];
+  T& operator () (int x, int y) {
+    return data[y * width + x];
   }
   bool valid(int x, int y) {
     return 0 <= x && x < width && 0 <= y && y < height;
   }
-  void clear(T value) {
+  void clear(const T& value) {
     for(int i = 0; i < width * height; i++) {
       data[i] = value;
     }
   }
 };
 
+enum dispType {
+  WALL,
+  PASSABLE,
+  WAY,
+};
+
+// 读取输入
 void readInput (int& width, int& height) {
   cerr << "Enter width and height" << endl;
   cerr << "Width: ";
@@ -78,38 +88,51 @@ void readInput (int& width, int& height) {
 int main () {
   int width, height;
   readInput(width, height);
-  assert(width > 0 && height > 0);
+  // 检查输入合法性
+  try {
+    if(!(width > 0 && height > 0)) throw("Width and height must be greater than 0");
+  } catch(const char* s) {
+    printf("Error: %s\n", s);
+    return 0;
+  }
 
+  // 分配空间
+  // nodes: 迷宫中的节点
+  // disp:  最终显示出的地图
+  // edges: 连接节点的边
   Array2d<Node> nodes(width, height);
   Array2d<int> disp((width * 2) + 1, (height * 2) + 1);
-  disp.clear(0);
+  disp.clear(dispType::WALL);
   vector<Edge> edges;
 
+  // 初始化地图
   for(int i = 0; i < height; i++) {
     for(int j = 0; j < width; j++) {
-      *disp.get(j * 2 + 1, i * 2 + 1) = 1;
+      disp(j * 2 + 1, i * 2 + 1) = dispType::PASSABLE;
     }
   }
-  *disp.get(0, 1) = *disp.get(disp.width - 1, disp.height - 2) = 2;
+  disp(0, 1) = disp(disp.width - 1, disp.height - 2) = dispType::WAY;
 
   for(int i = 0; i < height; i++) {
     for(int j = 0; j < width - 1; j++) {
-      edges.emplace_back(nodes.get(j, i), nodes.get(j + 1, i), j * 2 + 2, i * 2 + 1);
+      edges.emplace_back(&nodes(j, i), &nodes(j + 1, i), j * 2 + 2, i * 2 + 1);
     }
   }
   for(int i = 0; i < height - 1; i++) {
     for(int j = 0; j < width; j++) {
-      edges.emplace_back(nodes.get(j, i), nodes.get(j, i + 1), j * 2 + 1, i * 2 + 2);
+      edges.emplace_back(&nodes(j, i), &nodes(j, i + 1), j * 2 + 1, i * 2 + 2);
     }
   }
 
+  // 随机连边使图最终成为树
   random_shuffle(edges.begin(), edges.end());
   for(auto& e: edges) {
     if(e.destroy()) {
-      *disp.get(e.x, e.y) = 1;
+      disp(e.x, e.y) = dispType::PASSABLE;
     }
   }
 
+  // 通过BFS查找最短路径
   Array2d< pair<int, int> > parent(disp.width, disp.height);
   Array2d<bool> visited(disp.width, disp.height);
   visited.clear(0);
@@ -119,29 +142,40 @@ int main () {
     pair<int, int> p = bfsq.front();
     bfsq.pop();
     if(p == make_pair(0, 1)) break;
-    *visited.get(p.first, p.second) = 1;
+    visited(p.first, p.second) = 1;
     for(int i = -1; i <= 1; i++) {
-      if( parent.valid(p.first + i, p.second) && !*visited.get(p.first + i, p.second) && *disp.get(p.first + i, p.second) ) {
-        *parent.get(p.first + i, p.second) = make_pair(p.first, p.second);
+      if( parent.valid(p.first + i, p.second) && !visited(p.first + i, p.second) && disp(p.first + i, p.second) ) {
+        parent(p.first + i, p.second) = make_pair(p.first, p.second);
         bfsq.emplace(p.first + i, p.second);
       }
     }
     for(int i = -1; i <= 1; i++) {
-      if( parent.valid(p.first, p.second + i) && !*visited.get(p.first, p.second + i) && *disp.get(p.first, p.second + i) ) {
-        *parent.get(p.first, p.second + i) = make_pair(p.first, p.second);
+      if( parent.valid(p.first, p.second + i) && !visited(p.first, p.second + i) && disp(p.first, p.second + i) ) {
+        parent(p.first, p.second + i) = make_pair(p.first, p.second);
         bfsq.emplace(p.first, p.second + i);
       }
     }
   }
+  // 标记最短路径
   int curx = 0, cury = 1;
   while(make_pair(curx, cury) != make_pair(disp.width - 1, disp.height - 2)){
-    *disp.get(curx, cury) = 2;
-    tie(curx, cury) = *parent.get(curx, cury);
+    disp(curx, cury) = dispType::WAY;
+    tie(curx, cury) = parent(curx, cury);
   }
 
+  // 打印迷宫
   for(int i = 0; i < disp.height; i++) {
     for(int j = 0; j < disp.width; j++) {
-      cout << (*disp.get(j, i) == 1 ? " " : (*disp.get(j, i) == 2) ? "\e[0;31m█\e[0;0m" : "█");
+      cout << (disp(j, i) ? "  " : "██");
+    }
+    cout << endl;
+  }
+  // 等待用户确认后打印最短路
+  getchar();
+  getchar();
+  for(int i = 0; i < disp.height; i++) {
+    for(int j = 0; j < disp.width; j++) {
+      cout << (disp(j, i) == dispType::PASSABLE ? "  " : (disp(j, i) == dispType::WAY) ? "\e[0;31m██\e[0;0m" : "██");
     }
     cout << endl;
   }
